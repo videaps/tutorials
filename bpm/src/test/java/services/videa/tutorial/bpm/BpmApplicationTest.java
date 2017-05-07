953b27163e9287febda5746653822a6c5d5e372d
oliver <oliver.hock@videa-services.com>
2017-05-07 10:18:14 +0200

    rename test class for more simplicity

---
+++ bpm/src/test/java/services/videa/tutorial/bpm/BpmApplicationTest.java
@@ -0,0 +1,34 @@
+package services.videa.tutorial.bpm;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BpmApplicationTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public BpmApplicationTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(BpmApplicationTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+}
