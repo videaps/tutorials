d0326e2a16ca956ceea72cbafc5dd6bdd3873ab8
oliver <oliver.hock@videa-services.com>
2017-05-07 09:58:44 +0200

    set up bpm tutorial project

---
+++ bpm/src/main/java/services/videa/tutorial/bpm/BpmApplication.java
@@ -0,0 +1,14 @@
+package services.videa.tutorial.bpm;
+
+import org.camunda.bpm.application.ProcessApplication;
+
+/**
+ *
+ */
+@ProcessApplication("bpm")
+public class BpmApplication {
+	
+	public static void main(String[] args) {
+	}
+	
+}
